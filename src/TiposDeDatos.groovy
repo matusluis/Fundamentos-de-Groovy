f28b0af221e7795d3390f3658a90038de1a1a07b
matusluis <matusluis84@gmail.com>
2024-11-03 01:00:42 -0600

    final

--- src/TiposDeDatos.groovy
+++ src/TiposDeDatos.groovy
@@ -14,3 +14,25 @@ println saludo
 // Cadenas
 def saludo2 = "Hola, Lusillo saludos !"
 println saludo2
+
+// Listas
+def lista = [1, 2, 3, "cuatro", 5.0]
+println "Lista: $lista"
+
+// Listas2
+def Listas2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+println "Lista: $Listas2"
+
+// Listas3
+def Listas3 = ["pepe", "jose", "luis", "Raul"]
+println "Lista: $Listas3"
+
+// Mapas
+def mapa = [nombre: "Groovy", version: 3.0]
+println "Mapa: $mapa"
+println "Nombre en el mapa: ${mapa['nombre']}"
+
+// Mapas
+def mapa2 = [nombre: "luis", nombre2: "matus", version: 4.0, runtime: 2.0]
+println "Mapa: $mapa2"
+println "Nombre en el mapa: ${mapa2['nombre2']}"
