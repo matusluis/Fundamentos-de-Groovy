118a952332cf12db95182cd51a4a5aff10c99a4e
matusluis <matusluis84@gmail.com>
2024-11-03 00:41:22 -0600

    priemera pruebas

---
+++ src/TiposDeDatos.groovy
@@ -0,0 +1,16 @@
+// TiposDeDatos.groovy
+
+// Números
+def entero = 42
+def decimal = 3.14159
+println "\nEntero: $entero"
+println "Decimal: $decimal"
+
+
+// Cadenas
+def saludo = "Hola, Groovy!"
+println saludo
+
+// Cadenas
+def saludo2 = "Hola, Lusillo saludos !"
+println saludo2
